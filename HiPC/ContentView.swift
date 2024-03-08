ddf842da787cacc0c2ca781ea81a9b4f0cdea11c
danielcharry <daniel.charry@wolox.co>
2024-03-07 23:53:50 -0500

    up project

---
+++ HiPC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HiPC
+//
+//  Created by daniel.a.charry on 7/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HiPC/HiPCApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiPCApp.swift
+//  HiPC
+//
+//  Created by daniel.a.charry on 7/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HiPCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
